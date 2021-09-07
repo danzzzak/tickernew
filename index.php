8d0594b87e4443b1b911cbe4e7ae9c0a713899a6
danzzzak <siriuz2000@mail.ru>
2021-09-07 16:19:31 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.css" />
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.js"></script>
+    <script src="js.js"></script>
+    <title>tasksss</title>
+</head>
+<body>
+<div class="container box">
+   <h1 align="center">Live Add Edit Delete Datatables Records using PHP Ajax</h1>
+   <br />
+   <div class="table-responsive">
+   <br />
+    <div align="right">
+     <button type="button" name="add" id="add" class="btn btn-info">Add</button>
+    </div>
+    <br />
+    <div id="alert_message"></div>
+    <table id="user_data" class="table table-bordered table-striped">
+     <thead>
+      <tr>
+       <th>Frist Name</th>
+       <th>Last Name</th>
+       <th></th>
+      </tr>
+     </thead>
+    </table>
+   </div>
+  </div>
+</body>
+</html>
+
+<script type="text/javascript" language="javascript" >
+$(document).ready(function() {
+
+}
+</script>
